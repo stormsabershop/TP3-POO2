26df97baea707d4ecc96f80dc5cb700f81bb2566
jerome <2346784@cegeplimoilou.ca>
2024-03-25 12:05:31 -0400

    ajout des prduits

--- src/main/java/tp2/etudiant/Magasin.java
+++ src/main/java/tp2/etudiant/Magasin.java
@@ -5,12 +5,14 @@ import tp2.echange.Descriptible;
 import tp2.echange.Modele;
 import tp2.etudiant.boite.Boite;
 import tp2.etudiant.client.Achat;
+import tp2.etudiant.client.Panier;
 import tp2.etudiant.section.AireI;
 import tp2.etudiant.section.Entrepot;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 
 public class Magasin implements Modele {
@@ -18,15 +20,18 @@ public class Magasin implements Modele {
 
     private Collection<Achat> achats = new ArrayList<>();
 
+    private Panier panier;
+
 
     public Magasin() {
         // Instanciez les attributs nécessaires
-
+        panier = new Panier();
     }
 
 
     @Override
     public Collection<AireI> getAllSections() {
+
         return null;
     }
 
@@ -42,26 +47,40 @@ public class Magasin implements Modele {
     }
 
     public void placerProduits(Collection<Boite> boites, AireI section) {
-
+        Iterator<Boite> iterator = boites.iterator();
+        while (iterator.hasNext()){
+            section.placerProduits(iterator.next());
+        }
     }
 
     @Override
     public void mettreDansPanier(Collection<AbstractProduit> items) {
 
+        Iterator<AbstractProduit> iterator = items.iterator();
+        while (iterator.hasNext()){
+            panier.ajouteProduit(iterator.next());
+        }
+
     }
 
     @Override
     public Collection<AbstractProduit> getContenuPanier() {
-        return null;
+        return panier.getAchats();
     }
 
     @Override
     public void retirerDuPanier(List<AbstractProduit> itemARetirer) {
-
+        Iterator<AbstractProduit> iterator = itemARetirer.iterator();
+        while (iterator.hasNext()){
+            panier.retireProduit(iterator.next());
+        }
     }
 
     public Achat acheterPanier(String achateur, LocalDateTime dateTime) {
-        return null;
+        Achat achat = new Achat(achateur, panier, dateTime);
+        achats.add(achat);
+        panier.vide();
+        return achat;
     }
 
     @Override
--- src/main/java/tp2/etudiant/client/Panier.java
+++ src/main/java/tp2/etudiant/client/Panier.java
@@ -2,26 +2,35 @@ package tp2.etudiant.client;
 
 import tp2.application.AbstractProduit;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Panier {
 
+    private List<AbstractProduit> produits;
 
+    public Panier() {
+        this.produits = new ArrayList<>()  ;
+    }
 
     public void ajouteProduit(AbstractProduit produit) {
-
+        produits.add(produit);
     }
 
     public List<AbstractProduit> getAchats() {
-        return null;
+        return produits;
     }
 
 
     public void vide() {
-
+        Iterator<AbstractProduit> iterator = produits.iterator();
+        while (iterator.hasNext()){
+            produits.remove(iterator.next());
+        }
     }
 
     public boolean retireProduit(AbstractProduit produit){
-        return false;
+        return produits.remove(produit);
     }
 }
